70ab2f95802e4234e5d71878da473c06ebdc2f07
KatGaertner <97692910+KatGaertner@users.noreply.github.com>
2023-08-07 00:25:15 +0200

    added end to end test for feature 1

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,14 +1,66 @@
 import puppeteer from "puppeteer";
+import { getEvents } from "../api";
+
+const options = {
+  //   headless: false,
+  //   slowMo: 250,
+  //   timeout: 0,
+};
+
+describe("filter events by city", () => {
+  let browser;
+  let page;
+  beforeAll(async () => {
+    browser = await puppeteer.launch(options);
+    page = await browser.newPage();
+    await page.goto("http://localhost:3000/");
+    await page.waitForSelector(".event");
+  });
+
+  afterAll(() => {
+    browser.close();
+  });
+
+  test("When user hasn’t searched for a city, show upcoming events from all cities", async () => {
+    const EventListItems = await page.$$(".event");
+    expect(EventListItems.length).toBe(32);
+  });
+
+  test("User should see a list of suggestions when they search for a city.", async () => {
+    const city = "Berlin";
+    await page.type("#city-search input", city);
+    const suggestionListItems = await page.$$("#city-search li");
+    expect(suggestionListItems.length).toBe(2);
+  });
+
+  test("User can select a city from the suggested list.", async () => {
+    // const city = "Berlin";
+    // await page.type("#city-search input", city);
+    // these are not executed again here because of the test before
+    // --- keep in mind if this test needs to run alone ---
+
+    const suggestedCity = await page.$eval(
+      "#city-search li",
+      (el) => el.textContent
+    );
+    await page.click("#city-search li");
+    const inputValue = await page.$eval("#city-search input", (el) => el.value);
+    expect(inputValue).toBe(suggestedCity);
+
+    const EventListItems = await page.$$(".event");
+    const allEvents = await getEvents();
+    const berlinEvents = allEvents.filter(
+      (event) => event.location === suggestedCity
+    );
+    expect(EventListItems.length).toBe(berlinEvents.length);
+  });
+});
 
 describe("show/hide an event details", () => {
   let browser;
   let page;
   beforeAll(async () => {
-    browser = await puppeteer.launch({
-      //   headless: false,
-      //   slowMo: 250,
-      //   timeout: 0,
-    });
+    browser = await puppeteer.launch(options);
     page = await browser.newPage();
     await page.goto("http://localhost:3000/");
     await page.waitForSelector(".event");
